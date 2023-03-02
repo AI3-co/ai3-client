59a5c406ca995d6c7595a7cf32d2c466f5d14b53
apinanyogaratnam <apinanapinan@icloud.com>
2023-03-02 08:52:05 -0500

    fix: logo

--- src/Components/Partners/index.tsx
+++ src/Components/Partners/index.tsx
@@ -38,9 +38,9 @@ const Partners = ({ data: partners }: PartnersProps) => {
         name: 'Developer DAO',
         website: 'https://www.developerdao.com',
         // @ts-ignore
-        logo_dark: 'D_D_logo-light.svg',
+        logo_dark: 'D_D_logo-light-1.svg',
         // @ts-ignore
-        logo_light: 'D_D_logo-light.svg',
+        logo_light: 'D_D_logo-light-1.svg',
       },
     },
     {
